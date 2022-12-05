ede3a25b7b9755a5b8e48d4becaa7e026c341d0c
Bak8 <ataibegaliev@gmail.com>
2022-12-05 20:47:55 +0600

    Initial Commit

---
+++ HomeWork 3_5 ver2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork 3_5 ver2
+//
+//  Created by Atai Begaliev on 5/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
